2f58a84ae87930926c7638c49fcf4d71175fe6db
Pavel <gpv86x@gmail.com>
2021-08-19 16:16:18 +0300

    Всё согласно ТЗ по ДЗ1.

---
+++ js/main.js
@@ -0,0 +1,25 @@
+const products = [
+    { id: 1, title: 'Notebook', price: 2000 },
+    { id: 2, title: 'Mouse', price: 20 },
+    { id: 3, title: 'Keyboard', price: 200 },
+    { id: 4, title: 'Gamepad', price: 50 },
+];
+//Функция для формирования верстки каждого товара
+//Добавить в выводе изображение
+const renderProduct = (item) => {
+    return `<div class="product-item">
+                <h3>${item.title}</h3>
+                <img src="images/CartSVG.svg">
+                <p>${item.price} p.</p>
+                <button class="buy-btn">Купить</button>
+            </div>`
+};
+const renderPage = list => {
+    const productsList = list.map(item => renderProduct(item));
+    console.log(productsList);
+    document.querySelector('.products').innerHTML = productsList.join("");
+};
+
+renderPage(products);
+
+// Запятая в выводе появлялась как результат формарования массива, в котором по умолчанию разделителем служит запятая. Исправляется это методом "join()" с учазанием "отсутствия разделителя".
