70d26a899c92adb4f4b5d4cb3386f322f159db9e
Pavel <gpv86x@gmail.com>
2021-08-29 14:40:59 +0300

    Добавил получение корзины из .json

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
+const API = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses';
+
 class ProductList {
     constructor(container = '.products') {
         this.container = container;
         this.goods = [];
-        this._fetchProducts();
-        this.render(); //вывод товаров на страницу
-        this.sumPrice();
+        this._getProducts()
+            .then(data => { //data - объект js
+                this.goods = [...data];
+                this.render()
+            });
     }
-    _fetchProducts() {
-        this.goods = [
-            { id: 1, title: 'Notebook', price: 2000 },
-            { id: 2, title: 'Mouse', price: 20 },
-            { id: 3, title: 'Keyboard', price: 200 },
-            { id: 4, title: 'Gamepad', price: 50 },
-        ];
+
+    _getProducts() {
+
+        return fetch(`${API}/catalogData.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            })
     }
 
     render() {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
             const item = new ProductItem(product);
-
             block.insertAdjacentHTML("beforeend", item.render());
         }
-
     }
 
     sumPrice() {
@@ -31,15 +34,16 @@ class ProductList {
             result += item.price;
         }
         return result;
+        // return this.allProducts.reduce((accum, item) => accum += item.price, 0);
         // console.log(result);
     }
 }
 
 class ProductItem {
     constructor(product, img = 'images/CartSVG.svg') {
-        this.title = product.title;
-        this.id = product.id;
+        this.title = product.product_name;
         this.price = product.price;
+        this.id = product.id_product;
         this.img = img;
     }
     render() {
@@ -52,54 +56,66 @@ class ProductItem {
     }
 }
 
-let list = new ProductList();
-
 //класс корзины
-// class Cart {
-//     sumCartPrice();  //суммарная стоимость товаров в корзине
-//     clearCart();     //очистка корзины
-//     renderCart();    //вывод корзины на экран
-// };
+class Cart {
+    constructor(container = '.cart') {
+        this.container = container;
+        this.goods = [];
+        this._showCart();
+        this._getCartProducts()
+            .then(data => {
+                this.goods = [...data.contents];
+                this.render();
+            });
+    }
+
+    _getCartProducts() {
+        return fetch(`${API}/getBasket.json`)
+            .then(result => result.json())
+            .catch(error => {
+                console.log(error);
+            });
+    }
+
+    _showCart() {
+        document.querySelector(".btn-cart").addEventListener('click', () => {
+            document.querySelector(this.container).classList.toggle("invisible");
+        });
+    }
+
+    render() {
+        const block = document.querySelector(this.container);
+        for (let product of this.goods) {
+            const item = new CartItem(product);
+            block.insertAdjacentHTML("beforeend", item.render());
+        }
+    }
+
+};
 
 //класс товара в корзине
-// class CartItem {
-//     increaseItem();  //уменьшить количество товара
-//     removeItem();    //удалить товар из корзины
-//     decreaseItem();  //увеличить количество товара
-//     quantityItem();  //общее количество товаров одного вида
-//     sumPriceItem();  //сумма за товары одного вида
-// };
+class CartItem {
+    constructor(product, img = 'images/CartSVG.svg') {
+        this.title = product.product_name;
+        this.price = product.price;
+        this.id = product.id_product;
+        this.img = img;
+        this.quantity = product.quantity;
+    }
+    render() {
+        return `<div class="product-item">
+                <img src="${this.img}">
+                <div class="product-name">
+                <h3 class="title">${this.title}</h3>
+                <p class="price">${this.price} $</p>
+                </div>
+                <p class="quantity">${this.quantity} шт. на сумму ${this.price * this.quantity} $</p>
+                <button class="delete-btn">X</button>
+                <button class="add-btn">+</button>
+                <button class="remove-btn">-</button>
+            </div>`
+    }
+};
 
-
-
-
-
-
-
-
-
-
-
-
-
-// const products = [
-//     { id: 1, title: 'Notebook', price: 2000 },
-//     { id: 2, title: 'Mouse', price: 20 },
-//     { id: 3, title: 'Keyboard', price: 200 },
-//     { id: 4, title: 'Gamepad', price: 50 },
-// ];
-// const renderProduct = (item) => {
-//     return `<div class="product-item">
-//                 <h3>${item.title}</h3>
-//                 <img src="images/CartSVG.svg">
-//                 <p>${item.price} $</p>
-//                 <button class="buy-btn">Купить</button>
-//             </div>`
-// };
-// const renderPage = list => {
-//     const productsList = list.map(item => renderProduct(item));
-//     console.log(productsList);
-//     document.querySelector('.products').innerHTML = productsList.join("");
-// };
-
-// renderPage(products);
+let list = new ProductList();
+let cart = new Cart();
